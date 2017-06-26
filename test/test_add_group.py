ef549183913fc751660314096c703402f9399aa8
Alexander Khokhlov <alexander.xoxlov@gmail.com>
2017-06-26 13:35:40 +0300

    Added test scenario recorder with browser plugin
    Test script was updated after recording with firefox capabilities, used
    Firefox 45 ESR.

---
+++ test/test_add_group.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import unittest
+
+def is_alert_present(wd):
+    try:
+        wd.switch_to_alert().text
+        return True
+    except:
+        return False
+
+class TestGroup(unittest.TestCase):
+    def setUp(self):
+        caps = DesiredCapabilities.FIREFOX
+        caps['marionette'] = False
+        self.wd = webdriver.Firefox(capabilities=caps)
+        self.wd.implicitly_wait(60)
+    
+    def test_add_group(self):
+        success = True
+        wd = self.wd
+        wd.get("http://localhost/addressbook/")
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_css_selector("input[type='submit']").click()
+        wd.find_element_by_link_text("groups").click()
+        wd.find_element_by_name("new").click()
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys("NewGroup")
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys("NewGroupHeader")
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys("NewGroupFooter")
+        wd.find_element_by_name("submit").click()
+        wd.find_element_by_link_text("groups").click()
+        wd.find_element_by_link_text("Logout").click()
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").send_keys("\\undefined")
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").send_keys("\\undefined")
+        self.assertTrue(success)
+    
+    def tearDown(self):
+        self.wd.quit()
+
+if __name__ == '__main__':
+    TestGroup.main()
