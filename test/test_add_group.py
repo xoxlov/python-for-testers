b9a420f3c3bd4edee3de115c392c4d30b07362d6
Alexander Khokhlov <alexander.xoxlov@gmail.com>
2017-07-03 00:23:29 +0300

    Test structure and some functions were improved

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -24,27 +24,34 @@ class TestGroup(unittest.TestCase):
     
     def test_add_group(self):
         wd = self.wd
-        self.open_homepage(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
         self.create_group(wd, Group(name="NewGroup", header="NewGroupHeader", footer="NewGroupFooter"))
-        self.open_groups_page(wd)
         self.logout(wd)
 
     def test_add_empty_group(self):
         wd = self.wd
-        self.open_homepage(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
         self.create_group(wd, Group(name="", header="", footer=""))
-        self.open_groups_page(wd)
         self.logout(wd)
+
+    def login(self, wd, username, password):
+        self.open_homepage(wd)
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_css_selector("input[type='submit']").click()
 
     def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
 
     def create_group(self, wd, group):
+        self.open_groups_page(wd)
+        # init group creation
         wd.find_element_by_name("new").click()
+        # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
@@ -56,21 +63,14 @@ class TestGroup(unittest.TestCase):
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         wd.find_element_by_name("submit").click()
+        # return to the groups page
+        self.open_groups_page(wd)
 
     def open_groups_page(self, wd):
         wd.find_element_by_link_text("groups").click()
 
     def open_homepage(self, wd):
         wd.get("http://localhost/addressbook/")
-
-    def login(self, wd, username, password):
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_css_selector("input[type='submit']").click()
 
     def tearDown(self):
         self.wd.quit()
